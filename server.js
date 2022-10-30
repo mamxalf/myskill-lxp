e3dce17778b019d0726fd7b7629776095f84b7bc
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-10-31 02:50:30 +0700

    success deploy HI

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+// const cookieParser = require('cookie-parser')
+// const logger = require('morgan')
 
 const v1 = require('./src/app/routes/v1')
 
 const app = express()
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+// app.use(logger('dev'))
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cookieParser())
 
 app.get('/', function (req, res, next) {
   res.send('Hi!')
@@ -19,5 +19,10 @@ app.get('/', function (req, res, next) {
 //     router.use('/', v1)
 //   })
 // })
+
+const port = process.env.port || 8080
+app.listen(port, () => {
+  console.log(`listen on ${port}....`)
+})
 
 module.exports = app
