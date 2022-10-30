27cd388641125b5536d68ab9dc10b13deecafa7b
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-10-30 21:20:50 +0700

    clueless

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require('express')
-// const cookieParser = require('cookie-parser')
-// const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
 const v1 = require('./src/app/routes/v1')
 
 const app = express()
 
-// app.use(logger('dev'))
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
-// app.use(cookieParser())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res, next) {
   res.send('Hi!')
 })
-
-// Router V1
-// app.use('/api', v1)
-
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`)
-})
+// app.group('/api', (router) => {
+//   router.group('/v1', (router) => {
+//     router.use('/', v1)
+//   })
+// })
 
 module.exports = app
