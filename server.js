2702d1557db50c1a1d4ffe9bf2343c1432201319
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-10-31 02:59:11 +0700

    success deploy #2

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require('express')
-// const cookieParser = require('cookie-parser')
-// const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
 const v1 = require('./src/app/routes/v1')
 
 const app = express()
 
-// app.use(logger('dev'))
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
-// app.use(cookieParser())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.get('/', function (req, res, next) {
   res.send('Hi!')
 })
-// app.group('/api', (router) => {
-//   router.group('/v1', (router) => {
-//     router.use('/', v1)
-//   })
-// })
+app.use('/api', v1)
 
 const port = process.env.port || 8080
 app.listen(port, () => {
